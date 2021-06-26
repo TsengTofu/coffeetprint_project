eef24f39b443e5c535ddc3102bea5a88f5c19993
TofuTseng <tsengbatty@gmail.com>
2021-06-26 22:44:57 +0800

    [新增探索城市區塊] - ExploreCafeComponent、CityCafeCardComponent 兩個元件

---
+++ src/components/Front/Index/Explore/CityCafeCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <li>
+    <div class="card">
+  <img src="../../../../assets/images/city/taipei.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"><span class="material-icons-round">place</span>台北</h5>
+  </div>
+</div>
+  </li>
+  <!-- <img src="../../../../assets/images/city/taipei.png" alt=""> -->
+</template>
+
+<script>
+// 專門給探索區塊用的
+export default {
+  name: 'CityCafeCardComponent',
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {
+
+  },
+};
+</script>
+
+<style scoped lang="sass">
+.card
+  margin: 1rem
+  border: none
+  border-radius: 16px
+</style>
---
+++ src/components/Front/Index/Explore/ExploreCafe.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container-fluid explore_cafe_wrapper">
+    <h4><span class="material-icons-round">place</span>探索城市</h4>
+    <p>一起探索各地的咖啡廳吧！</p>
+    <!-- 區域選擇的清單 -->
+    <div class="area_block">
+      <ul class="list-unstyled d-flex">
+        <CityCafeCardComponent v-for="index in 4" :key="index"/>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import CityCafeCardComponent from './CityCafeCard.vue';
+
+// 專門給探索區塊用的
+export default {
+  name: 'ExploreCafeComponent',
+  props: {},
+  components: {
+    CityCafeCardComponent,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {
+
+  },
+};
+</script>
+
+<style scoped lang="sass">
+.explore_cafe_wrapper
+  background: #406B5A
+  padding: 20px 20px 150px
+  .area_block
+    max-width: 1200px
+    margin: 0 auto
+</style>
