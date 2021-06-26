b210e852d7af24ebf1b576ce9f380f80ef7ad053
TofuTseng <tsengbatty@gmail.com>
2021-06-26 22:50:52 +0800

    [首頁初步排版] - LandingPageComponent 初步切版 - 手機版尚未處理

---
+++ src/components/Front/Index/LandingPage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="landing_page">
+    <div class="d-flex flex-column align-items-start title_block">
+      <p class="main_title"><span class="material-icons-round">near_me</span>尋找你的咖啡廳</p>
+      <p class="vice_title">還在煩惱要去哪發呆、聚餐、放鬆嗎？</p>
+      <div class="input-group searchbar">
+        <input type="text" class="form-control"
+          placeholder="請輸入欲搜索地區" aria-label="請輸入欲搜索地區" aria-describedby="button-addon2">
+        <button class="btn btn-primary" type="button" id="button-addon2">搜尋</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // 首頁最上面那個 Scroll
+  name: 'LandingPageComponent',
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped lang="sass">
+.landing_page
+  position: relative
+  top: 0px
+  z-index: 3
+  background-position: center
+  background-image: url('../../../assets/images/index_bg.png')
+  background-size: cover
+  background-attachment: fixed
+  display: block
+  width: 100%
+  min-height: 100vh
+  .title_block
+    color: #FFFFFF
+    padding: 200px 0 0 150px
+    width: 50%
+    .main_title
+      font-size: 2rem
+      font-weight: 700
+    .vice_title
+      font-size: 1rem
+</style>
