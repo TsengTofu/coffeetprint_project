807ff7774c3a8f6321cefe7f96c5afa0897cb7bc
TofuTseng <tsengbatty@gmail.com>
2021-06-28 00:29:42 +0800

    [首頁產品卡片]新增咖啡廳卡片元件 - 後續要跟產品列表頁的卡片元件整合成同一個

---
+++ src/components/Front/Index/CafeProduct/CafeCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <li>
+    <div class="card">
+      <img src="../../../../assets/images/cafe_list/PIEPAI.jpeg" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <span class="material-icons-round">favorite_border</span>
+        <h5 class="card-title">日暖咖啡雙人套餐</h5>
+        <!-- 星星跟地點 -->
+        <div class="location_stars">
+          <p><span class="material-icons-round">place</span>台北｜台灣</p>
+          <span class="material-icons-round">star</span>
+          <span class="material-icons-round">star_border</span>
+        </div>
+        <p class="card-text">
+          主打企鵝飯糰的日式早午餐店，票券內容包含：雙份企鵝飯糰早午餐，套餐包含飲料與蛋糕⋯
+        </p>
+        <!-- 剩下幾張與價格 -->
+        <div class="d-flex justify-content-between bottom_block">
+          <!-- 剩下幾張 -->
+          <p class="rest_num">
+            剩下 <span>10</span> 張
+          </p>
+          <!-- 價格 -->
+          <p class="price">
+            NT$<span>1580</span>
+          </p>
+        </div>
+      </div>
+      <a href="#" class="btn btn-primary d-flex justify-content-center add_to_cart">
+          <span class="material-icons-round">shopping_cart</span>加入購物車
+        </a>
+    </div>
+  </li>
+</template>
+<script>
+export default {
+  name: 'CafeCardComponent',
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {},
+};
+</script>
+<style scoped lang="sass">
+li
+  width: 25%
+  .card
+    margin: 1rem
+    border-radius: 16px
+    .add_to_cart
+      width: 100%
+      position: relative
+      bottom: 0px
+      left: 0px
+      border-radius: 0px 0px 16px 16px
+</style>
---
+++ src/components/Front/Index/CafeProduct/CafeList.vue
@@ -0,0 +1,56 @@
+<template>
+  <!-- TODO  這塊可能會有重複，記得跟 Front 那邊比對，如果可以共用就放在 Core -->
+  <!-- NOTE  是首頁喔！ -->
+  <div class="container-fluid cafe_list_block">
+    <h4><span class="material-icons-round">coffee</span>發現咖啡廳</h4>
+    <p>一起看看哪些咖啡廳有提供優惠券吧！</p>
+    <!-- 產品列表清單 -->
+    <div class="list_wrap">
+      <ul class="list-unstyled d-flex flex-wrap">
+        <!-- 放卡片元件 -->
+        <CafeCardComponent v-for="index in 7" :key="index" />
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import CafeCardComponent from './CafeCard.vue';
+
+export default {
+  name: 'CafeListComponent',
+  components: {
+    CafeCardComponent,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {},
+};
+</script>
+
+<style scoped lang="sass">
+// 這段是為了要加上 wave
+.cafe_list_block
+  background: #F0EBE6
+  position: relative
+  width: 100%
+  padding: 0px
+  z-index: 50
+  &::before
+      content: ''
+      background-position: top
+      background-image: url('../../../../assets/images/wave_coffee.svg')
+      background-size: cover
+      display: block
+      width: 100%
+      height: 150px
+      position: absolute
+      top: -130px
+      z-index: -1
+  .list_wrap
+    width: 100%
+    max-width: 1200px
+    margin: 0 auto
+</style>
