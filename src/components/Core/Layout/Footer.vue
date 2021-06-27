522693c5b48b6ed2d6731d8f015c6de3cf457fee
TofuTseng <tsengbatty@gmail.com>
2021-06-28 00:25:56 +0800

    [更新 Footer]改變 Footer 排版

--- src/components/Core/Layout/Footer.vue
+++ src/components/Core/Layout/Footer.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="footer_wrapper">
-      <div class="content">
-        <p>Copyright © 2020 CoffeetPrint.</p>
-        <div class="detail">
-          <p> 僅個人作品練習，無商業用途。</p>
-          <ul>
-              <!-- 放那隻貓的 icon -->
-              <li><a href="">Github</a></li>
-              <!-- Linkdn icon -->
-              <li><a href="">Linkedin</a></li>
-          </ul>
-        </div>
+    <div class="d-flex flex-row align-items-center justify-content-between content">
+      <a class="navbar-brand logo" href="#"></a>
+      <p>Copyright © 2020 CoffeetPrint. 僅個人作品練習，無商業用途。</p>
+      <div class="detail">
+        <ul class="list-unstyled d-flex link">
+          <li>
+            <p><span class="material-icons-round">account_circle</span>後台登入</p>
+          </li>
+          <li>
+            <a href="">
+              <img src="../../../assets/images/icon/github.svg" alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <img src="../../../assets/images/icon/linkedin.svg" alt="" />
+            </a>
+          </li>
+        </ul>
       </div>
+    </div>
   </div>
 </template>
 
@@ -31,7 +40,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="sass">
 .footer_wrapper
-  background: #000
+  background: #535252
   width: 100%
   color: #fff
   display: flex
@@ -43,11 +52,28 @@ export default {
     justify-content: center
     flex-direction: column
     padding: 1rem
+    .logo
+      width: 160px
+      height: 40px
+      display: block
+      background-position: center
+      background-image: url('../../../assets/images/logo_coffeetprint.svg')
+      background-size: contain
+      background-repeat: no-repeat
+    p
+      margin: 0px
+      font-size: 0.875rem
     .detail
       display: flex
       justify-content: center
       margin: .5rem 0
-      ul
-        display: flex
+      .link
         li
+          a
+            width: 20px
+            height: 20px
+            display: block
+            img
+              width: 100%
+              height: auto
 </style>
