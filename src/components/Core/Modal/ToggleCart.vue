6f18d5cda2dc1261b14a112c8ae08850b5d8749c
TofuTseng <tsengbatty@gmail.com>
2021-07-04 14:23:27 +0800

    [側邊欄購物車] - 處理資料不同步的問題 - 用 mitt 解決

--- src/components/Core/Modal/ToggleCart.vue
+++ src/components/Core/Modal/ToggleCart.vue
@@ -19,29 +19,67 @@
     </div>
     <!-- offcanvas 裡面的內容 -->
     <div class="offcanvas-body">
-      <p>這邊要做一個假的購物車</p>
-      <img src="../../../assets/images/empty_cart.svg" alt="">
+      <div v-if="cart_list">
+        <CartListComponent
+          :cart_list="cart_list"
+          :getCartList="getCartList"
+        />
+      </div>
       <!-- 按鈕們 -->
       <button type="button" class="btn-primary" @click="directToPage('cart')">訂單結帳</button>
-      <button type="button" class="btn-primary" @click="directToPage('products')">繼續購物</button>
+      <!-- 這個按鈕之後要改掉 -->
+      <button type="button" class="btn-primary" >繼續購物</button>
     </div>
   </div>
 </template>
 
 <script>
+import CartListComponent from '../../Front/Cart/Data/CartList.vue';
+
 export default {
   name: 'ToggleCartComponent',
   props: {},
+  components: {
+    CartListComponent,
+  },
   data() {
-    return {};
+    return {
+      cart_list: '',
+    };
   },
   methods: {
     directToPage(pageName) {
-      console.log(pageName);
       this.$router.push(`/${pageName}`);
     },
+    getCartList() {
+      const requestUrl = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_API_PATH}/cart`;
+      this.axios
+        .get(requestUrl)
+        .then((response) => {
+          if (response.data.success) {
+            console.log('成功抓到購物車列表的資料', response.data.data);
+            //  TODO  單就購物車的列表，不包含總金額 total, final_total
+            this.cart_list = response.data.data.carts;
+            if (this.cart_list.length === 0) {
+              this.$emit('disabledNextStep');
+            }
+          } else {
+            console.log('出了點錯誤，請稍後再嘗試，謝謝。');
+          }
+        })
+        .catch((error) => {
+          console.log(error, 'getDataError');
+        });
+    },
   },
-  mounted() {},
+  created() {
+    this.emitter.on('updateCartList', () => {
+      this.getCartList();
+    });
+  },
+  mounted() {
+    this.getCartList();
+  },
 };
 </script>
 
