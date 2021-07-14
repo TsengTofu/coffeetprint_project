f42a4656acd36f2c95510588f6ee7f7a7a0918aa
TofuTseng <tsengbatty@gmail.com>
2021-07-14 08:02:17 +0800

    [新增收藏清單的元件]

---
+++ src/views/Front/Favorite.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wrapper">
+    <p>加到最愛頁面</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+<style lang="sass" scoped>
+.wrapper
+  margin: 120px 0
+</style>
