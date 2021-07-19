37de35b6f1a4c4d4b6c7f5aa40c16e99d3ae6d7b
TofuTseng <tsengbatty@gmail.com>
2021-07-19 23:02:34 +0800

    [首頁]加入訂閱區塊

--- src/views/Front/Entry.vue
+++ src/views/Front/Entry.vue
@@ -5,15 +5,17 @@
   <ExploreCafeComponent />
   <!-- 多做一個不同的分類排版 -->
   <!-- 產品部分露出 -->
-  <!--  TODO  這個名稱要改掉，資料改透過外面取得 -->
   <CafeListWrapperComponent />
+  <SubscribeComponent />
 </template>
 
 <script>
+// 按照順序排
 import LandingPageComponent from '../../components/Front/Index/LandingPage.vue';
 import CafePromotionCarouselComponent from '../../components/Front/Index/Promotion/CafePromotionCarousel.vue';
 import ExploreCafeComponent from '../../components/Front/Index/Explore/ExploreCafe.vue';
 import CafeListWrapperComponent from '../../components/Front/Index/CafeProduct/CafeListWrapper.vue';
+import SubscribeComponent from '../../components/Front/About/Subscribe/Subscribe.vue';
 
 export default {
   name: 'Entry',
@@ -22,6 +24,7 @@ export default {
     CafePromotionCarouselComponent,
     ExploreCafeComponent,
     CafeListWrapperComponent,
+    SubscribeComponent,
   },
 };
 </script>
