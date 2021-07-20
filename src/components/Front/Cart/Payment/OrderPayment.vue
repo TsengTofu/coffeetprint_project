f77316a7fdf7c79b222b5a58bad55e30d15c6918
TofuTseng <tsengbatty@gmail.com>
2021-07-21 00:05:52 +0800

    [訂購後的確認頁面]流程都還沒放上去，資料還沒撈完

--- src/components/Front/Cart/Payment/OrderPayment.vue
+++ src/components/Front/Cart/Payment/OrderPayment.vue
@@ -1,23 +1,47 @@
 <template>
   <div class="card">
     <!-- 這邊要多一個功能，可以直接複製訂單編號 -->
-    <p>{{ orderDetail.id }}</p>
-    <!-- 這邊要做日期的轉換 -->
-    <p>建立時間：{{ order_time }}</p>
-    <p>是否已經付款：{{ orderDetail.is_paid }}</p>
-    <p>訂單總金額：{{ orderDetail.total }}</p>
-    <!-- 這塊還需要去做拆分 -->
-    <p>使用者資料：{{ orderDetail.user }}</p>
+    <div class="card">
+      <!-- <img src="" class="card-img-top" alt="..."> -->
+      <div class="card-body">
+        <h5 class="card-title">訂單 {{ orderDetail.id }} 等待付款中！</h5>
+        <!--  TODO  這邊要做日期的轉換 -->
+        <figcaption class="blockquote-footer">結帳時間：{{ order_time }}</figcaption>
+        <div class="card-text">
+          <!-- 這裡面是卡片的內容 -->
+          <!-- 是否已經付款 -->
+          <b v-if="orderDetail.is_paid">已付款</b>
+          <b v-else>未付款</b>
+          <hr>
+          <div>
+            訂單總金額：NT$ <span>{{ Math.floor(orderDetail.total) }}</span>
+          </div>
+          <!-- 使用者資料 -->
+          <h5>使用者資料</h5>
+          <div v-if="orderDetail.user">
+            <p>訂購人地址：{{ orderDetail.user.address }}</p>
+            <p>訂購人信箱：{{ orderDetail.user.email }}</p>
+            <p>訂購人姓名：{{ orderDetail.user.name }}</p>
+            <p>訂購人電話：{{ orderDetail.user.tel }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- NOTE  -->
+
     <!-- 訂單資料 -->
     <!-- TODO  這邊要確認一下 API -->
     <br>
     <template v-for="(item, key) in product_list" :key="item + key">
       <div class="card">
-        測試一下：{{ orderDetail.products[item] }}<br>
+        <!-- FIXME  這邊應該會是明天要處理的重點 -->
+        <!-- 測試一下：{{ orderDetail.products[item] }}<br> -->
+        <p>優惠券資料：{{ orderDetail.products[item].coupon }}<br></p>
       </div>
     </template>
     <!-- 這樣可以拿到 key 值 -->
-    <!-- <p>{{ Object.keys(orderDetail.products) }}</p> -->
+    <!-- 可以拿到購買的品項 -->
+    <p>{{ Object.keys(orderDetail.products) }}</p>
     <button
       type="button"
       class="btn btn-primary"
