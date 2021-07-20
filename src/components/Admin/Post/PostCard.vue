9247ce9f7788fc9eee70556c3b717f9f35aaa001
TofuTseng <tsengbatty@gmail.com>
2021-07-21 00:01:52 +0800

    [後台貼文的修正]版型陽春，但功能正常 - 無法刪除文章

--- src/components/Admin/Post/PostCard.vue
+++ src/components/Admin/Post/PostCard.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <p>這是貼文編輯的地方</p>
-     <div class="modal fade" id="couponModal" ref="modal" tabindex="-1">
+  <!-- 這邊改成換頁的方式處理 -->
+  <!--  FIXME  這邊要再補上編輯的功能 -->
+  <div class="modal fade" id="couponModal" ref="modal" tabindex="-1">
     <div
       class="modal-xl
         modal-dialog
@@ -26,30 +26,184 @@
           </span>
         </div>
         <div class="modal-body outer">
-          <div class="container">
-            <!--  TODO  這邊要放內容 -->
+          <!-- 這段是正常的 -->
+          <div class="row">
+            <div class="col-sm-4">
+              <div class="mb-3">
+                <label for="title" class="form-label">標題</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="title"
+                  v-model="tempArticle.title"
+                  placeholder="請輸入標題"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="image" class="form-label">輸入圖片網址</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="image"
+                  v-model="tempArticle.imageUrl"
+                  placeholder="請輸入圖片連結"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="author" class="form-label">作者</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="author"
+                  v-model="tempArticle.author"
+                  placeholder="請輸入標題"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="create_at">文章建立日期</label>
+                <input
+                  type="date"
+                  class="form-control"
+                  id="create_at"
+                  v-model="create_at"
+                />
+              </div>
+            </div>
+            <div class="col-sm-8">
+              <label for="tag" class="form-label">標籤</label>
+              <div class="row gx-1 mb-3">
+                <div
+                  class="col-md-2 mb-1"
+                  v-for="(label, key) in tempArticle.tag"
+                  :key="key"
+                >
+                  <div class="input-group input-group-sm">
+                    <input
+                      type="text"
+                      class="form-control form-control"
+                      id="tag"
+                      v-model="tempArticle.tag[key]"
+                      placeholder="請輸入標籤"
+                    />
+                    <button
+                      type="button"
+                      class="btn btn-outline-danger"
+                      @click="tempArticle.tag.splice(key, 1)"
+                    >
+                      <i class="bi bi-x"></i>
+                    </button>
+                  </div>
+                </div>
+                <div
+                  class="col-md-2 mb-1"
+                  v-if="
+                    tempArticle.tag[tempArticle.tag.length - 1] ||
+                    !tempArticle.tag.length
+                  "
+                >
+                  <button
+                    class="btn btn-outline-primary btn-sm d-block w-100"
+                    @click="tempArticle.tag.push('')"
+                  >
+                    新增標籤
+                  </button>
+                </div>
+              </div>
+              <div class="mb-3">
+                <label for="description" class="form-label">文章描述</label>
+                <textarea
+                  type="text"
+                  class="form-control"
+                  id="description"
+                  v-model="tempArticle.description"
+                  placeholder="請輸入文章描述"
+                ></textarea>
+              </div>
+              <div class="mb-3">
+                <ckeditor
+                  :editor="editor"
+                  :config="editorConfig"
+                  v-model="tempArticle.content"
+                ></ckeditor>
+              </div>
+              <div class="mb-3">
+                <div class="form-check">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    v-model="tempArticle.isPublic"
+                    id="isPublic"
+                  />
+                  <label class="form-check-label" for="isPublic">
+                    是否公開
+                  </label>
+                </div>
+              </div>
+            </div>
           </div>
+          <!-- 這段是正常的 -->
         </div>
         <div class="modal-footer button_block">
           <button class="cancel" data-bs-dismiss="modal" type="button">取消</button>
-          <button v-if="status === 'post'" @click="addNewProduct" type="button">確認</button>
-          <button v-else @click="editProduct" type="button">確認</button>
+          <button v-if="status === 'post'" @click="addNewPost" type="button">確認</button>
+          <button v-else @click="editPost" type="button">確認</button>
         </div>
       </div>
     </div>
   </div>
-  </div>
 </template>
 <script>
+import { Modal } from 'bootstrap';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
 export default {
   //  TODO  卡片是不是要改成 Modal 的形容會比較好？
   name: 'PostCardComponent',
-  props: {},
+  props: {
+    status: String,
+  },
   components: {},
   data() {
-    return {};
+    return {
+      modal: '',
+      tempArticle: {
+        tag: [''],
+        create_at: 1555459220,
+      },
+      //  FIXME  這邊的日期要做轉換，目前是都先寫死
+      create_at: 1555459220,
+      // 參考：https://ckeditor.com/docs/ckeditor5/latest/builds/guides/integration/frameworks/vuejs-v3.html#editor
+      editor: ClassicEditor,
+      editorConfig: {
+        toolbar: ['heading', 'typing', 'bold', 'italic', '|', 'link'],
+      },
+    };
   },
-  methods: {},
-  mounted() {},
+  methods: {
+    //  TODO  所以還要取得一次 content，單筆文章的資料
+    // 新增一篇文章
+    addNewPost() {
+      const requestUrl = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_API_PATH}/admin/article`;
+      console.log(requestUrl);
+      console.log(this.tempArticle);
+      this.axios
+        .post(requestUrl, { data: this.tempArticle })
+        .then((response) => {
+          if (response.data.success) {
+            // 要把資料傳遞出去
+            // this.$emit('emit-data', this.tempProduct);
+            this.modal.hide();
+          } else {
+            console.log('出了點錯誤，請稍後再嘗試，謝謝。');
+          }
+        })
+        .catch((error) => {
+          console.log(error, 'deleteProductAPIError');
+        });
+    },
+  },
+  mounted() {
+    this.modal = new Modal(this.$refs.modal);
+  },
 };
 </script>
--- src/components/Admin/Post/PostList.vue
+++ src/components/Admin/Post/PostList.vue
@@ -1,14 +1,108 @@
 <template>
   <div>
     <p>貼文的清單</p>
+    <!-- 懶得做 UI 了 -->
+    {{ post_list }}
+    <table class="table table-striped table-hover">
+       <thead>
+         <tr>
+           <th>訂單編號</th>
+           <th>建立時間</th>
+           <th>編輯</th>
+           <th>刪除</th>
+         </tr>
+      </thead>
+      <tbody>
+        <template v-if="post_list.length > 0">
+          <tr v-for="(post, key) in post_list" :key="'post_' + key">
+            <td>{{ post.id }}</td>
+            <!-- <td>{{ $dayjs.unix(order.create_at).format('YYYY-MM-DD') }}</td> -->
+            <!-- 編輯這邊改成換頁面 -->
+            <td>
+              <button class="icon_btn"
+                type="button"
+                @click="directToCurrentPostEdit(post.id)"
+              >
+                <span class="material-icons-round">edit</span>
+              </button>
+            </td>
+            <td>
+              <button class="icon_btn"
+                type="button"
+                data-bs-toggle="modal"
+                data-bs-target="#confirmModal"
+                @click="targetItem = post"
+              >
+                <span class="material-icons-round">delete</span>
+              </button>
+            </td>
+          </tr>
+        </template>
+      </tbody>
+    </table>
+    <!--  NOTE @@@  -->
+     <button class="add_new_product_button"
+        type="button"
+        data-bs-toggle="modal"
+        data-bs-target="#myModal"
+        @click="setStatus('post', {})"
+      >
+        <span class="material-icons-round">add</span> 新增產品
+      </button>
+    <PostCardComponent
+      ref="modal"
+      id="myModal"
+      :status="status"
+      :article_content="article_content"
+    />
   </div>
 </template>
 <script>
+import PostCardComponent from './PostCard.vue';
+
 export default {
   name: 'PostListComponent',
   props: {},
+  components: {
+    PostCardComponent,
+  },
   data() {
-    return {};
+    return {
+      post_list: [],
+      status: '',
+      // 暫時使用的 content
+      article_content: '',
+      targetItem: {},
+      targetId: '',
+    };
+  },
+  methods: {
+    // 導頁
+    directToCurrentPostEdit(currentPostId) {
+      this.$router.push(`/admin/post/detail/${currentPostId}`);
+    },
+    getPostList() {
+      //  TODO  這邊要記得放 page，page 的參數要改掉
+      // 取得文章列表
+      const requestUrl = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_API_PATH}/articles?page=1`;
+      this.axios.get(requestUrl)
+        .then((response) => {
+          console.log('response', response);
+          this.post_list = response.data.articles;
+        })
+        .catch((error) => {
+          console.log('error', error);
+        });
+      // 注意：文章列表中不包含 content 屬性，請使用單筆文章 API 取得。
+    },
+    // 設定 status
+    setStatus(axiosMethod, data) {
+      this.status = axiosMethod;
+      this.$refs.modal.tempArticle = JSON.parse(JSON.stringify(data));
+    },
+  },
+  mounted() {
+    this.getPostList();
   },
 };
 </script>
