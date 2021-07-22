5bc4b720f531a90cad2269e899452c90b3034e00
TofuTseng <tsengbatty@gmail.com>
2021-07-22 21:30:50 +0800

    [調整版面]Footer 樣式

--- src/components/Core/Layout/Footer.vue
+++ src/components/Core/Layout/Footer.vue
@@ -1,22 +1,26 @@
 <template>
-  <div class="footer_wrapper">
-    <div class="d-flex flex-row align-items-center justify-content-between content">
-      <a class="navbar-brand logo" href="#"></a>
-      <p>Copyright © 2020 CoffeetPrint. 僅個人作品練習，無商業用途。</p>
-      <div class="detail">
-        <ul class="list-unstyled d-flex link">
-          <li class="admin_login">
-            <router-link to="/login" target="_blank">
+  <div class="footer_wrapper container-fluid">
+    <div class="row content">
+      <a
+        class="navbar-brand logo col-sm-2"
+        href="#"
+      >
+      </a>
+      <p class="col-auto">Copyright © 2020 CoffeetPrint. 僅個人作品練習，無商業用途。</p>
+      <div class="detail col-sm-2">
+        <ul class="list-unstyled row link">
+          <li class="admin_login col-8">
+            <router-link to="/login" target="_blank" class="d-flex">
               <span class="material-icons-round">account_circle</span>
               後台登入
             </router-link>
           </li>
-          <li>
+          <li class="col-sm-2">
             <a href="https://github.com/TsengTofu" target="_blank">
               <img src="../../../assets/images/icon/github.svg" alt="" />
             </a>
           </li>
-          <li>
+          <li class="col-sm-2">
             <a href="">
               <img src="../../../assets/images/icon/linkedin.svg" alt="" />
             </a>
@@ -42,39 +46,40 @@ export default {
 <style scoped lang="sass">
 .footer_wrapper
   background: #535252
-  width: 100%
+  // width: 100%
   color: #fff
-  display: flex
+  // display: flex
   .content
-    width: 100%
-    max-width: 1200px
-    margin: 0 auto
-    display: flex
-    justify-content: center
-    flex-direction: column
+    // width: 100%
+    // max-width: 1200px
+    // margin: 0 auto
+    // display: flex
+    // justify-content: center
+    // flex-direction: column
     padding: 1rem
     .logo
-      width: 160px
+      // width: 160px
       height: 40px
       display: block
       background-position: center
       background-image: url('../../../assets/images/logo_coffeetprint.svg')
       background-size: contain
       background-repeat: no-repeat
+      margin: 0px
     p
       margin: 0px
       font-size: 0.875rem
     .detail
-      display: flex
-      justify-content: center
-      margin: .5rem 0
+      // justify-content: center
+      // margin: .5rem 0
       .link
         li
           &.admin_login
             a
-              display: flex
+              // width: 100%
+              // display: flex
               width: fit-content
-              text-decoration: none
+              // text-decoration: none
           a
             width: 20px
             height: 20px
